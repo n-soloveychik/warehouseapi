a504eaec234228c0e2a95657853b536be1c3bcc1
Evgenii Nikitin <regemdeu@gmail.com>
2020-06-27 12:58:34 +0300

    add comments

--- app/Internal/OrderMaster/ItemMaster.php
+++ app/Internal/OrderMaster/ItemMaster.php
@@ -28,6 +28,13 @@ class ItemMaster
         $this->invoiceMaster = new InvoiceMaster($item->invoice);
     }
 
+    /**
+     * @param Invoice $invoice
+     * @param ItemTemplate $itemTemplate
+     * @param int $count
+     * @param string $lot
+     * @return Item
+     */
     public static function make(Invoice $invoice, ItemTemplate $itemTemplate, int $count, string $lot) : Item{
         return Item::create([
             'status_id' => ItemStatus::AWAIT_DELIVERY,
@@ -43,10 +50,20 @@ class ItemMaster
         ]);
     }
 
+    /**
+     * @param Item $item
+     * @return bool
+     * @throws \Exception
+     */
     public static function delete(Item $item) : bool {
         return $item->delete();
     }
 
+    /**
+     * @param Item $item
+     * @param null $count
+     * @return int
+     */
     public static function updateStatus(Item $item, $count=null) : int {
         $m = new ItemMaster($item);
         if ($count === null){
@@ -72,6 +89,11 @@ class ItemMaster
         return $item->status_id;
     }
 
+    /**
+     * @param $item
+     * @param int $count
+     * @throws \Exception
+     */
     public static function shipment($item, int $count){
         if ($item->count_in_stock < $count){
             throw new \Exception("count_in_stock < count_shipment");
@@ -88,6 +110,11 @@ class ItemMaster
 
     }
 
+    /**
+     * @param Item $item
+     * @param iterable $images
+     * @param string $description
+     */
     public static function newClaim(Item $item, Iterable $images, string $description){
         $imgModels = [];
         foreach ($images as $image){
@@ -110,6 +137,9 @@ class ItemMaster
         $m->setStatusClaim();
     }
 
+    /**
+     * @param int $statusId
+     */
     public function writeStatus(int $statusId){
         $this->item->status_id = $statusId;
         $this->item->save();
@@ -122,6 +152,9 @@ class ItemMaster
         $this->writeStatus(ItemStatus::CLAIMS);
     }
 
+    /**
+     * @param int $count
+     */
     public function updateCount(int $count){
         if ($count > $this->item->count){
             $count = $this->item->count;
@@ -139,6 +172,10 @@ class ItemMaster
         $this->item->save();
     }
 
+    /**
+     * @param Item $item
+     * @return bool
+     */
     public static function hasClaims(Item $item) : bool {
         foreach ($item->claims as $claim){
             if($claim->closed == 0)
@@ -147,8 +184,9 @@ class ItemMaster
         return false;
     }
 
-
-
+    /**
+     * @throws OrderMasterException
+     */
     protected function failIfItemHaveClaims(){
         if (self::hasClaims($this->item))
             throw new OrderMasterException('Item has unclosed claims', Response::HTTP_CONFLICT);
