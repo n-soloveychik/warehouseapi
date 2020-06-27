f41606bb8b3d2e49777c9f8953576632159dc0f8
Evgenii Nikitin <regemdeu@gmail.com>
2020-06-27 12:41:59 +0300

    shpment category

--- app/Http/Controllers/V1/Warehouse/InvoiceController.php
+++ app/Http/Controllers/V1/Warehouse/InvoiceController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\V1\Warehouse;
 
 use App\Http\Controllers\Controller;
 use App\Internal\OrderMaster\InvoiceMaster;
+use App\Internal\OrderMaster\ItemMaster;
 use App\Internal\ResponseFormatters\Formatter\ItemFormatter;
 use App\Models\Invoice;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class InvoiceController extends Controller
 {
@@ -21,12 +23,29 @@ class InvoiceController extends Controller
         return response(null, Response::HTTP_OK);
     }
 
+    /**
+     * @param $invoice_id
+     * @param $category_id
+     * @return array
+     */
     public function shipmentCategory($invoice_id, $category_id){
         $inv = Invoice::with(['items'=>function($q) use ($category_id){
             $q->where('category_id', $category_id);
         }])
             ->where('invoice_id', $invoice_id)
             ->first();
+
+        if (empty($inv)){
+            throw new NotFoundHttpException('Invoice undefined');
+        }
+
+        // Отгрузка
+        $inv->items->each(function ($item){
+            if ($item->count == $item->count_in_stock) {
+                ItemMaster::shipment($item, $item->count);
+            }
+        });
+
         return [
             'category_id' => (int)$category_id,
             'invoice_id' => (int)$invoice_id,
