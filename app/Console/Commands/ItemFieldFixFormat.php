91b0951f18f6a1b82ddc683724d30da903f82637
Evgenii Nikitin <regemdeu@gmail.com>
2020-06-30 01:18:49 +0300

    add fix item field command

---
+++ app/Console/Commands/ItemFieldFixFormat.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ItemFieldFixFormat extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'item:ff';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'fix fields';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
