dcdf0f6efcdb8556a752d1d6506efe474e1fee54
Evgenii Nikitin <regemdeu@gmail.com>
2021-01-27 16:24:48 +0300

    telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('telescope:prune')->daily();
-        //$schedule->command('telescope:prune --hours=48')->daily();
+        $schedule->command('telescope:prune')->weekly();
+        $schedule->command('telescope:clear')->weekly();
     }
 
     /**
